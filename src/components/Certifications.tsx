ae6841f38284c7671f310e71bdee90b4ecac971c
Aswin K <153754552+AswinK06@users.noreply.github.com>
2025-06-29 19:42:37 +0530

    Update Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -90,7 +90,7 @@ const Certifications = () => {
         <div className="text-center mb-14">
   <div className="flex justify-center items-center gap-4">
     <Trophy className="text-purple-600 dark:text-purple-300 animate-bounce" size={32} />
-    <h2 className="text-5xl md:text-6xl font-extrabold bg-gradient-to-r from-purple-600 to-indigo-600 dark:from-purple-300 dark:to-indigo-400 bg-clip-text text-transparent">
+    <h2 className="text-5xl md:text-6xl font-bold bg-gradient-to-r from-purple-600 to-indigo-600 dark:from-purple-300 dark:to-indigo-400 bg-clip-text text-transparent">
             Professional Certifications
           </h2>
     <Trophy className="text-purple-600 dark:text-purple-300 animate-bounce" size={32} />
